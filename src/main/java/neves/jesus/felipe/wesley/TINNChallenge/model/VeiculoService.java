e2a195273e68939873ae3cfcc83b8bab9b8defe2
Wesley Neves <wesley.neves@wexinc.com>
2021-04-13 16:38:35 -0300

    remoção de import desnecessário

--- src/main/java/neves/jesus/felipe/wesley/TINNChallenge/model/VeiculoService.java
+++ src/main/java/neves/jesus/felipe/wesley/TINNChallenge/model/VeiculoService.java
@@ -2,7 +2,6 @@ package neves.jesus.felipe.wesley.TINNChallenge.model;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
